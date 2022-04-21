b8ea8691cbad8dcd48bedf523ceecc1be7d6020e
TaraRK <tararezaei03@gmail.com>
2022-04-21 19:03:36 +0430

    first commit

---
+++ src/Enums/MenuEnums.java
@@ -0,0 +1,26 @@
+package Enums;
+
+import java.util.regex.Pattern;
+
+public enum MenuEnums {
+    ENTER("menu enter (?<menuName>Login|Main|PlayGame|Profile)"),
+    SHOWCURRENT("menu show-current"),
+    CREATEUSER("user create (--username|-u) (?<username>[A-Za-z0-9]+) (--nickname|-n) (?<nickname>[A-Za-z0-9]+) (--password|-p) (?<password>[A-Za-z0-9]+)" ),
+    USERLOGIN("user login (--username|-u) (?<username>[A-Za-z0-9]+) (--password|-p) (?<password>[A-Za-z0-9]+)"),
+    USERLOGOUT("user logout"),
+    PLAYGAME("play game (--player1|-p1) (?<username>[A-Za-z0-9]+).*"),
+    CHANGE_NICKNAME("profile change (--nickname|-n) (?<newNickname>[A-Za-z0-9]+)"),
+    CHANGE_PASSWORD("profile change (--password|-p) (--current|-c) (?<currentPassword>[A-Za-z0-9]+) (--new|-n) (?<newPassword>[A-Za-z0-9]+)"),
+
+    ;
+
+    private String regex;
+    MenuEnums (String regex)
+    {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return regex;
+    }
+}
---
+++ src/Model/Database.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Database {
+    private ArrayList<User> Users;
+
+    public Database ()
+    {
+        this.Users = new ArrayList<>();
+    }
+
+    public void addUser ( User user)
+    {
+        this.Users.add(user);
+    }
+
+    public ArrayList<User> getUsers()
+    {
+        return Users;
+    }
+
+    public User getUserByUsernameAndPassword (String username, String password)
+    {
+        for ( User user : this.Users)
+        {
+            if ( user.getUsername().equals(username) && user.getPassword().equals(password))
+            {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User getUserByUsername(String username)
+    {
+        for ( User user : this.Users)
+        {
+            if (user.getUsername().equals(username))
+            {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public User getUserByNickname(String nickname)
+    {
+        for ( User user : this.Users)
+        {
+            if (user.getNickname().equals(nickname))
+            {
+                return user;
+            }
+
+        }
+        return null;
+    }
+    
+}
