0409a30ba48334c03ebeab0c0509bdd104e3d37f
ehsan <ehsan@ehsans-MacBook-Pro.local>
2022-04-24 22:36:21 +0430

    finished map checkpoint

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,25 +10,7 @@ import Model.User;
 public class Main{
     public static void main(String[] args) {
     
-      Map map = new Map();
-      map.initializeMap();
-      ArrayList<Terrain> terrains = new ArrayList<Terrain>();
-      for(int i = 0; i < 25;i++){
-        for(int j = 0; j < 16;j++){
-          terrains.add(map.getTerrain()[i][j]);
-        }
-      }
-      Civilization civilization = new Civilization(terrains, null, 0, 0, "A");
-      User user = new User(null, null, null, civilization);
-      Database database = new Database();
-      database.addUser(user);
-      String [][]  result = map.printMap(database, user);
-      for(int i = 0; i < 25;i++){
-        for(int j = 0; j < 6;j++){
-          System.out.println(result[i][j]);
-        }
-      }
-
+  
 
     }
 }
