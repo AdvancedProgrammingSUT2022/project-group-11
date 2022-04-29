18a8c9c99a9f0b547986053c6975007a2177efdf
ehsan <ehsan@ehsans-MacBook-Pro.local>
2022-04-29 19:53:47 +0430

    first test edit

--- src/test/java/MAP/MapGeneratorTest.java
+++ src/test/java/MAP/MapGeneratorTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -19,30 +20,18 @@ import Model.User;
 
 @ExtendWith(MockitoExtension.class)
 public class MapGeneratorTest {
-   
-  //  @Mock
-  //  Database database;
-  //  @Mock
-  //  User user;
- //   @Mock
-  //  String[][] Printmap;
-    @Mock
-    ArrayList<River> rivers;
-   
+    
     @Mock
     River river;
     @Mock
     Terrain terrainOne;
     @Mock
     Terrain terrainTwo;
-    @Mock 
-    Map map;
     @Test
     public void hasRiverTest() {
-      GameMapGenerator mapGenerator = new GameMapGenerator(map.getTerrain(), map.getRiver(), 32, 16, 6);
-       River checkRiver = mapGenerator.hasRiver(terrainOne, terrainTwo);
+       Map map = new Map();
+       River checkRiver = map.hasRiver(terrainOne, terrainTwo);
        Assertions.assertNull(checkRiver);
-      
     }
 
 }
