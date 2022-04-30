f3abeafa4ad4e13af140ec0bdd6690a2f7a3067e
ehsan <ehsan@ehsans-MacBook-Pro.local>
2022-04-30 09:59:43 +0430

    fix test

---
+++ src/test/java/ControllerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import Model.User;
+
+public class ControllerTest{
+
+    @Mock
+    User user;
+    
+
+    @ExtendWith(MockitoExtension.class)
+    public void createUser(){
+
+    }
+    
+}
--- src/test/java/MAP/MapGeneratorTest.java
+++ src/test/java/MAP/MapGeneratorTest.java
@@ -1,37 +1,43 @@
 package MAP;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import Model.Database;
-import Model.GameMapGenerator;
 import Model.Map;
 import Model.River;
 import Model.Terrain;
-import Model.User;
 
 @ExtendWith(MockitoExtension.class)
 public class MapGeneratorTest {
-    
+
     @Mock
     River river;
     @Mock
     Terrain terrainOne;
     @Mock
     Terrain terrainTwo;
-    @Test
+    @Mock
+    ArrayList<River> rivers;
+
+
+
+    
     public void hasRiverTest() {
-       Map map = new Map();
-       River checkRiver = map.hasRiver(terrainOne, terrainTwo);
-       Assertions.assertNull(checkRiver);
+        Map map = new Map();
+        rivers = new ArrayList<River>();
+        river = new River(terrainOne, terrainTwo);
+        rivers.add(river);
+        when(river.getFirstTerrain()).thenReturn(terrainOne);
+        when(river.getSecondTerrain()).thenReturn(terrainTwo);
+        River checkRiver = map.hasRiver(terrainOne, terrainTwo);
+        assertNotNull(checkRiver);
     }
 
 }
