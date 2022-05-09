291da15bb980c50cea7b3e0e5a23012f1bc95e19
essi2002 <naderiiehsan6@gmail.com>
2022-05-09 14:48:25 +0430

    longswordsman

--- src/test/java/MAP/MapGeneratorTest.java
+++ src/test/java/MAP/MapGeneratorTest.java
@@ -2582,4 +2582,128 @@ public class MapGeneratorTest {
             cityController.createUnit(matcher, city);
         }
     }
+
+    // LONGSWORDSMAN
+
+    @Test
+    public void createUnitLONGSNotEnoughMoney(){
+        Civilization civil = new Civilization(0, 3, "A");
+        City city = new City(null, civil,null , 3, null,0, 0, null);
+        city.setGold(0);
+        String input = "CONSTRUCT UNIT LONGSWORDSMAN";
+        Matcher matcher;
+        CityController cityController = new CityController();
+        if((matcher = getMatcher(input, GameEnums.CREATE_UNIT)) != null){
+            cityController.createUnit(matcher, city);
+        }
+    }
+
+    @Test
+    public void createuitLONGSLackResource(){
+        Resource resource = new Resource(ResourceTypes.BANANAS);
+        CombatUnit combatunit = new CombatUnit(3, 4, 0, 0, 0, 0, false, false,
+        UnitTypes.SETTLER, true, false, false, false, false);
+        Improvement improvement = new Improvement(3, 4,ImprovementTypes.FARM);
+        Terrain central = new Terrain(3, 4, "visible", TerrainTypes.PLAINS, new ArrayList<>(), combatunit, null,
+                improvement, resource,
+                new ArrayList<Revealed>());
+
+      
+        Civilization civil = new Civilization(100, 3, "A");
+        ArrayList<Technology> tech = new ArrayList<>();
+        Technology technology = new Technology(false, 0, UnitTypes.CHARIOT_ARCHER.getTechnologyRequirements(),false);
+        tech.add(technology);
+        civil.setTechnologies(tech);
+  
+        City city = new City(null, civil,central , 3, null,0, 0, null);
+        city.setGold(1000);
+        city.setCombatUnit(combatunit);
+        String input = "CONSTRUCT UNIT LONGSWORDSMAN";
+        Matcher matcher;
+        CityController cityController = new CityController();
+        if((matcher = getMatcher(input, GameEnums.CREATE_UNIT)) != null){
+            cityController.createUnit(matcher, city);
+        }
+    }
+
+    @Test
+    public void createuitLONGSANrtECH(){
+        Resource resource = new Resource(UnitTypes.LONGSWORDSMAN.getResourceRequirements());
+        CombatUnit combatunit = new CombatUnit(3, 4, 0, 0, 0, 0, false, false,
+        UnitTypes.SETTLER, true, false, false, false, false);
+        Improvement improvement = new Improvement(3, 4,ImprovementTypes.FARM);
+        Terrain central = new Terrain(3, 4, "visible", TerrainTypes.PLAINS, new ArrayList<>(), combatunit, null,
+                improvement, resource,
+                new ArrayList<Revealed>());
+
+      
+        Civilization civil = new Civilization(100, 3, "A");
+        ArrayList<Technology> tech = new ArrayList<>();
+      //  Technology technology = new Technology(false, 0, UnitTypes.CATAPULT.getTechnologyRequirements(),false);
+     //   tech.add(technology);
+        civil.setTechnologies(tech);
+  
+        City city = new City(null, civil,central , 3, null,0, 0, null);
+        city.setGold(1000);
+        city.setCombatUnit(combatunit);
+        String input = "CONSTRUCT UNIT LONGSWORDSMAN";
+        Matcher matcher;
+        CityController cityController = new CityController();
+        if((matcher = getMatcher(input, GameEnums.CREATE_UNIT)) != null){
+            cityController.createUnit(matcher, city);
+        }
+    }
+    @Test
+    public void createuitLONGScombat(){
+        Resource resource = new Resource(UnitTypes.LONGSWORDSMAN.getResourceRequirements());
+        CombatUnit combatunit = new CombatUnit(3, 4, 0, 0, 0, 0, false, false,
+        UnitTypes.SETTLER, true, false, false, false, false);
+        Improvement improvement = new Improvement(3, 4,ImprovementTypes.FARM);
+        Terrain central = new Terrain(3, 4, "visible", TerrainTypes.PLAINS, new ArrayList<>(), combatunit, null,
+                improvement, resource,
+                new ArrayList<Revealed>());
+
+      
+        Civilization civil = new Civilization(100, 3, "A");
+        ArrayList<Technology> tech = new ArrayList<>();
+        Technology technology = new Technology(false, 0, UnitTypes.LONGSWORDSMAN.getTechnologyRequirements(),false);
+        tech.add(technology);
+        civil.setTechnologies(tech);
+        City city = new City(null, civil,central , 3, null,0, 0, null);
+        city.setGold(10000);
+        city.setCombatUnit(combatunit);
+        String input = "CONSTRUCT UNIT LONGSWORDSMAN";
+        Matcher matcher;
+        CityController cityController = new CityController();
+        if((matcher = getMatcher(input, GameEnums.CREATE_UNIT)) != null){
+            cityController.createUnit(matcher, city);
+        }
+        
+    }
+    @Test
+    public void createuitLONGSNnormal(){
+        Resource resource = new Resource(UnitTypes.LONGSWORDSMAN.getResourceRequirements());
+        CombatUnit combatunit = new CombatUnit(3, 4, 0, 0, 0, 0, false, false,
+        UnitTypes.SETTLER, true, false, false, false, false);
+        Improvement improvement = new Improvement(3, 4,ImprovementTypes.FARM);
+        Terrain central = new Terrain(3, 4, "visible", TerrainTypes.PLAINS, new ArrayList<>(), combatunit, null,
+                improvement, resource,
+                new ArrayList<Revealed>());
+
+      
+        Civilization civil = new Civilization(100, 3, "A");
+        ArrayList<Technology> tech = new ArrayList<>();
+        Technology technology = new Technology(false, 0, UnitTypes.LONGSWORDSMAN.getTechnologyRequirements(),false);
+        tech.add(technology);
+        civil.setTechnologies(tech);
+        City city = new City(null, civil,central , 3, null,0, 0, null);
+        city.setGold(10000);
+        city.setCombatUnit(null);
+        String input = "CONSTRUCT UNIT LONGSWORDSMAN";
+        Matcher matcher;
+        CityController cityController = new CityController();
+        if((matcher = getMatcher(input, GameEnums.CREATE_UNIT)) != null){
+            cityController.createUnit(matcher, city);
+        }
+    }
 }
