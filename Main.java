4bcce9d848173141d8954206f95068124d751874
Mani Tahami <85440257+mani1381@users.noreply.github.com>
2022-05-10 00:01:06 +0430

    Add files via upload

--- Main.java
+++ Main.java
@@ -123,4 +123,79 @@ public class Main {
 
 
     }
+
+    @Test
+    public void setImprovementTest() {
+        Civilization civil = new Civilization(0, 3, "A");
+        Map map = new Map();
+        terrains = new Terrain[map.getROW()][map.getCOL()];
+        for (int i = 0; i < map.getROW(); i++) {
+            for (int j = 0; j < map.getCOL(); j++) {
+                CombatUnit combatunit = new CombatUnit(i, j, 0, 0, 0, 0, false, false,
+                        UnitTypes.SETTLER, true, false, false, false, false);
+                Improvement improvement = new Improvement(i, j, ImprovementTypes.FARM);
+                terrains[i][j] = new Terrain(i, j, "visible", TerrainTypes.PLAINS, new ArrayList<>(), combatunit, null,
+                        improvement, null,
+                        new ArrayList<Revealed>());
+
+            }
+        }
+        map.setTerrains(terrains);
+        User user = new User(null, null, null, civil);
+        Database database = new Database();
+        database.addUser(user);
+        database.setMap(map);
+        DatabaseController databaseController = new DatabaseController(database);
+        ArrayList<User> allUser = new ArrayList<>();
+        allUser.add(user);
+        GameMenu gamemenu = new GameMenu(databaseController, allUser);
+        Matcher matcher;
+        String input = "SET CHEAT IMPROVEMENT CAMP 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT FARM 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+
+        input = "SET CHEAT IMPROVEMENT MINE 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT PLANTATION 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT PASTURE 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT LUMBERBILL 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT MANUFACTURY 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT QUARRY 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT RAILROAD 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT ROAD 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        input = "SET CHEAT IMPROVEMENT TRADINGPOST 4 3";
+        if ((matcher = GameEnums.getMatcher(input, GameEnums.SET_CHEAT_RESOURCE)) != null) {
+            gamemenu.setCheatResource(matcher);
+        }
+        
+
+    }
  */
