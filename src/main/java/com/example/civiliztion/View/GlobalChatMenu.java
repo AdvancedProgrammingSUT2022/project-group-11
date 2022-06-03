08c1df11973fccaf12dcce85864ae6f862fadba7
essi2002 <naderiiehsan6@gmail.com>
2022-06-03 13:52:07 +0430

    public chatroom

--- src/main/java/com/example/civiliztion/View/GlobalChatMenu.java
+++ src/main/java/com/example/civiliztion/View/GlobalChatMenu.java
@@ -1,4 +1,25 @@
 package com.example.civiliztion.View;
 
+import com.example.civiliztion.Model.User;
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
 public class GlobalChatMenu {
+
+    private static GlobalChatMenu instance;
+    @FXML
+    private VBox allMessages;
+
+    public static GlobalChatMenu getInstance(){
+        if(instance == null){
+            instance = new GlobalChatMenu();
+        }
+        return instance;
+    }
+
+    private User user;
+
+
+
+    
 }
